597f0d46b2e2be2cc05936957669b2dacb61f7eb
yester <yester.huang@gmail.com>
2015-03-23 17:54:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,59 @@ app.get('/list', function(req,res){
     }
 );
 
+//paging list
+
+
+//select video data, star data, tag data by video id,
+
+//if you have the route /user/:name, then the “name” property is available as req.params.name. This object defaults to {}.
+// GET /user/tj
+//req.params.name
+// => "tj"
+app.get('/video/:id', function(req,res){
+
+        var sqlite3 = require('sqlite3');
+        sqlite3.verbose();
+
+        var db = new sqlite3.Database('db/videos.db');
+
+
+        var listJSON ={};
+        var i = 0;
+        var video_id = req.params.id;
+        db.all("select id, name from tb_videos where id="+video_id, function(err, rows){
+
+            rows.forEach(function(row){
+
+                var itemJSON = {};
+                itemJSON.id= row.id;
+                itemJSON.name = row.name;
+                listJSON[i]= itemJSON;
+                i++;
+            });
+
+            //res.send('list:'+output);
+
+            res.json(listJSON);
+            //res.json({ user: 'tobi' })
+        });
+
+        db.close();
+    }
+);
+
+
+
+// GET /search?q=tobi+ferret
+// req.query.q
+// => "tobi ferret"
+
+// GET /search?tag=1, it will return videos with tag =1
+// req.query.tag ==> 1
+
+
+
+
 var server = app.listen(3000, function(){
  console.log('Listening on port %d', server.address().port);
 
